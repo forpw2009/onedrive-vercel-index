2564821f6a9c85c58d95619068642baf59bef4dd
lpssxs <731588359@qq.com>
2022-03-28 15:38:42 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -35,7 +35,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://npccc.ga" target="_blank" rel="noopener noreferrer">onedrive</a>. Made with ❤ by NPC.',
+    'Powered by <a href="https://one.npccc.ga" target="_blank" rel="noopener noreferrer">onedrive</a>. Made with ❤ by NPC.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
